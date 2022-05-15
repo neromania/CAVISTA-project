0852ecd301e49a10d8d490a755ef9418e1221869
Takkal Ismail <takkalismael@gmail.com>
2022-05-15 23:13:44 +0200

    adding the nav-tabs and fixing the searchinBar

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const list = [];
 function descriptionBottle(element) {
         const imgBottle = document.getElementsByTagName('img')[1];
         const nameTitle = document.getElementById("nameWine"); 
+        const hashtagId = document.getElementById("idWine");
         const grape = document.getElementById("grape");
         const country = document.getElementById("country");
         const region = document.getElementById("region");
@@ -10,7 +11,9 @@ function descriptionBottle(element) {
         const color = document.getElementById("color");
         const price = document.getElementById("price");
         const like = document.getElementById("like");
+        const desc = document.getElementById("nav-description");
         imgBottle.src = 'https://cruth.phpnet.org/epfc/caviste/public/pics/'+element.picture;
+        hashtagId.innerHTML = '#'+element.id;
         nameTitle.innerHTML = element.name;
         grape.innerHTML = '<em>Grapes</em> : '+element.grapes;
         country.innerHTML = '<em>Country</em> : '+element.country;
@@ -19,12 +22,27 @@ function descriptionBottle(element) {
         capacity.innerHTML = '<em>Capacity</em> : '+element.capacity;
         color.innerHTML = '<em>Color</em> : '+element.color;
         price.innerHTML = '<em>Price</em> : '+element.price+" €";
+        desc.innerHTML = element.description;
 }
 
+function getComments(wineId){
+    const comments = document.getElementById("nav-comments");
+    comments.innerHTML = "";
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function (e){
+        const doc = this.responseText;
+        const data = JSON.parse(doc);
+        for(let i = 0; i < data.length; i++){
+            comments.innerHTML += '<p>user n° '+data[i].user_id+' -> <em">'+data[i].content+'"</em></p>';
+        }
+    };
+    xhr.open ('GET','https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/'+wineId+'/comments',true);
+    xhr.send();
+
+}
 function getTotalLike(wineId){
     const likeCount = document.getElementById("likeCount");
-    
-    //return 345;
+
     const xhr = new XMLHttpRequest();
     xhr.onload = function (e){
         const doc = this.responseText;
@@ -34,6 +52,7 @@ function getTotalLike(wineId){
     xhr.open ('GET','https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/'+wineId+'/likes-count',true);
     xhr.send();
 }
+
 
 const xhr = new XMLHttpRequest();
 
@@ -53,6 +72,7 @@ xhr.onload = function (){
         }
 
         let items = document.querySelectorAll('li');
+        $("#liste li").hide().show('slow');
 
         for(let item of items){ 
             item.onclick = function () {
@@ -62,6 +82,7 @@ xhr.onload = function (){
                             //console.log(wine.name);
                             getTotalLike(wine.id);
                             descriptionBottle(wine);
+                            getComments(wine.id);
                         }
                     }
                 }
@@ -71,8 +92,9 @@ xhr.onload = function (){
            
                 btnSearch.onclick = function(){
                     liste.innerHTML = '';
+                    
                 for(let i of list){
-                    if (i.name.indexOf((inputSearch.value).toUpperCase()) != -1) {
+                    if (i.name.toLowerCase().indexOf(inputSearch.value) != -1) {
                         liste.innerHTML += '<li id="'+i.id+'">'+i.name+'</li>';
                     }
                     let items = document.querySelectorAll('li');
@@ -81,8 +103,9 @@ xhr.onload = function (){
                         item.onclick = function () {
                             for(let wine of list){
                                 if (wine.name.indexOf(this.innerHTML) != -1) {
-                                    //console.log(find.name);
-                                    descriptionBottle(wine);                                    
+                                    descriptionBottle(wine);    
+                                    getTotalLike(wine.id);        
+                                    getComments(wine.id);                        
                                 }
                             }
                             }
